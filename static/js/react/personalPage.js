fb412e549c8d2c7891699c8407f5ee53320f9556
kak-rastoki <mr.9hen@gmail.com>
2024-10-03 21:08:40 +0700

    WIP: PP -> Иконки, фикс разметки

--- static/js/react/personalPage.js
+++ static/js/react/personalPage.js
@@ -4,11 +4,12 @@ function PP_LeftMenu ({onMenuClick}) {
 
   return (
     <>
-      <button class = "pp-menu-bt" onClick = {()=> onMenuClick('settings')}>Настройки</button>
-      <button class = "pp-menu-bt" onClick={() => onMenuClick('favorites')}>Избранное</button>
-      <button class = "pp-menu-bt" onClick={() => onMenuClick('orders')}>История заказов</button>
-      <button class = "pp-menu-bt" onClick={() => onMenuClick('following')}>Подписки</button>
-      <button class = "pp-menu-bt exit-bt" onClick={() => onMenuClick('exit')}>Выйти из аккаунта</button>
+
+      <button class = "pp-menu-bt" onClick = {()=> onMenuClick('settings')}><i class="fa fa-address-card" aria-hidden="true"></i> Настройки</button>
+      <button class = "pp-menu-bt" onClick={() => onMenuClick('favorites')}><i class="fa fa-heart" aria-hidden="true"></i> Избранное</button>
+      <button class = "pp-menu-bt" onClick={() => onMenuClick('orders')}><i class="fa fa-history" aria-hidden="true"></i> История заказов</button>
+      <button class = "pp-menu-bt" onClick={() => onMenuClick('following')}><i class="fa fa-user-circle" aria-hidden="true"></i> Подписки</button>
+      <button class = "pp-menu-bt exit-bt" onClick={() => onMenuClick('exit')}><i class="fa fa-sign-out" aria-hidden="true"></i> Выйти из аккаунтa</button>
     </>
   );
 }
@@ -17,13 +18,13 @@ function PP_Content ({activeMenu}) {
   const renderContent = () => {
     switch (activeMenu){
       case 'settings':
-        return <div class="h-pp-content">Настройки профиля</div>;
+        return <div class="h-pp-content"><i class="fa fa-address-card" aria-hidden="true"></i> Настройки профиля</div>;
       case 'favorites':
-        return <div class="h-pp-content">Избранное</div>;
+        return <div class="h-pp-content"><i class="fa fa-heart" aria-hidden="true"></i> Избранное</div>;
       case 'orders':
-         return <div class="h-pp-content">История заказов</div>;
+         return <div class="h-pp-content"><i class="fa fa-history" aria-hidden="true"></i> История заказов</div>;
       case 'following':
-        return <div class="h-pp-content">Ваши подписки</div>;
+        return <div class="h-pp-content"><i class="fa fa-user-circle" aria-hidden="true"></i> Ваши подписки</div>;
       default:
         return <div class ="h-pp-content">Выберите пункт в меню</div>;
     }
@@ -39,6 +40,13 @@ function PP_Content ({activeMenu}) {
   );
 
 }
+
+function Settings () {
+
+
+}
+
+
 function PersonalPage () {
   // получение состояния
   const [activeMenu, setActiveMenu] = React.useState('settings');
